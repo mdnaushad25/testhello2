937546ea3a9e0edb0e58e58768c2615655cc959a
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-23 18:56:27 +0530

    pack name corrected

--- actions/getusersmessage.py
+++ actions/getusersmessage.py
@@ -3,7 +3,7 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 
-    def run(self, a,b):
+    def run(self,a,b):
         c=a+b
         print(c)
         return(True,c)
