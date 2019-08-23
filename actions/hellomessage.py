5d322502771878272c0b0885822d95dd83305308
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-23 18:00:31 +0530

    After change test

--- actions/hellomessage.py
+++ actions/hellomessage.py
@@ -4,6 +4,5 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, message):
-        message="hi how are you. very soon give me information"
         print(message)
         return(True,message)
