2dc3d537d83cbf290d363f338a09c13b86c3c1f5
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-26 17:06:06 +0530

    pack arthmetic operation corrected

--- actions/minmaxtesting.py
+++ actions/minmaxtesting.py
@@ -4,8 +4,8 @@ class minmaxclass(Action):
     def checkminmax(self,a,b):
         if a>b:
             print("a is greate than b and c")     
-            else:
-                print("c is greate than a and b")
+        else:                
+            print("c is greate than a and b")
       
                 
 
