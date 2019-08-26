d70af156f14b77dbe3ddbcca325fc58d3e7a4a2c
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-26 15:13:24 +0530

    pack arthmetic operation corrected

--- actions/getusersmessage.py
+++ actions/getusersmessage.py
@@ -1,11 +1,9 @@
 import sys
 from st2common.runners.base_action import Action
 
-class MyAction(Action):
+class getusersmessage(Action):
 
     def run(self,a,b):
-        var1:int=a
-        var2:int=a
-        var3=var1+var2     
-        print(var3)
-        return(True,var3)
+        sum=int(a)+int(b) 
+        print(sum)
+        return(True,sum)
