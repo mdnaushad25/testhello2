a8579e4aa92c1bf45577c6f2d876d2e059d6716e
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-26 14:58:54 +0530

    pack arthmetic operation corrected

--- actions/getusersmessage.py
+++ actions/getusersmessage.py
@@ -1,17 +1,11 @@
 import sys
-import pandas as pd
 from st2common.runners.base_action import Action
 
 class MyAction(Action):
 
     def run(self,a,b):
-        # c= int(a)+int(b)
-        # print("The sum of {0} and {1} is {2}" .format(a, b, c)) 
-# num1 = 1.5
-# num2 = 6.3
-# Add two numbers
-        sum = int(a) + int(b)
-# Display the sum
-        print('The sum of {0} and {1} is {2}'.format(a, b, sum))
-        # print(c)
-        return(True,sum)
+        var1:int=a
+        var2:int=a
+        var3=var1+var2     
+        print(var3)
+        return(True,var3)
