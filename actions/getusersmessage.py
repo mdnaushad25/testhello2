caa4c766cf449dbb61c4dade14b9326f00a71fbd
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-26 10:41:18 +0530

    pack arthmetic operation corrected

--- actions/getusersmessage.py
+++ actions/getusersmessage.py
@@ -5,5 +5,7 @@ class MyAction(Action):
 
     def run(self,a,b):
         c= int(a)+int(b)
-        print(c)
+        print("The sum of {0} and {1} is {2}" .format(a, b, c)) 
+
+        # print(c)
         return(True,c)
