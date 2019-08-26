d0b015b669ac113647b998cbc52a326807416953
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-26 17:22:16 +0530

    pack arthmetic operation corrected

--- actions/minmaxtesting.py
+++ actions/minmaxtesting.py
@@ -3,9 +3,10 @@ from st2common.runners.base_action import Action
 class minmaxclass(Action):
     def checkminmax(self,a,b):
         if a>b:
-            print("a is greate than b and c")     
+            print("a is greate than b and c")                 
         else:                
             print("c is greate than a and b")
+        return(True,a)
       
                 
 
