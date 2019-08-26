882eda2c308ba6372f94347dcf40af00539fc72c
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-26 17:55:31 +0530

    pack arthmetic operation corrected

--- actions/minmaxwithfourvalue.py
+++ actions/minmaxwithfourvalue.py
@@ -3,7 +3,11 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
     def run(self,a,b,c,d):
         if a>b and a>c and a>d:
-            print("a is greate than b and c")                 
-        else:                
-            print("c is greate than a and b")
+            print("a is greate than b c and d")                 
+        elif b>a and b>c and b>d:                
+            print("b is greate than a c and d")
+        elif c>a and c>d and c>d:                
+            print("c is greate than a b and d")
+        else
+            print("d is greate than a b and c")
         return(True)
