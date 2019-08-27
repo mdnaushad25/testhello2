2cffc4295d2139230fad25dde2420bc67aad0b98
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-27 13:09:05 +0530

    pack arthmetic operation corrected

--- actions/getpattern.py
+++ actions/getpattern.py
@@ -3,7 +3,12 @@ from st2common.runners.base_action import Action
 class WritePattern(Action):
     def run(self,a):
         i = 1
+        l=1
+       
         while i < a:
-            print("*")
+            while l<a:
+                print(" ")
+            while i<a:
+                print("*")
             i += 1
         return(True)
