567cb32b5029c8c46c290da4a0fab37adbc5f433
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-27 17:20:46 +0530

    pack arthmetic operation corrected

--- actions/fillvalue.py
+++ actions/fillvalue.py
@@ -2,7 +2,7 @@ import sys
 from st2common.runners.base_action import Action
 class MyAction(Action):
     def run(self,a,b):
-        allvalue="Subject: "+a+"  Marks: "+b
+        allvalue=allvalue+"Subject: "+a+"  Marks: "+b
         print(allvalue)
         return(True)
     
