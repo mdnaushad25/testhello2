8a7fcfe110e7502d3af4222240619703034584cb
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-27 15:31:47 +0530

    pack arthmetic operation corrected

--- actions/getpattern.py
+++ actions/getpattern.py
@@ -6,6 +6,7 @@ class WritePattern(Action):
         for i in range(1,a):
             for j in range(1,i+1):
                 print("*")
+                print("*")
             print("\r")
             
             
