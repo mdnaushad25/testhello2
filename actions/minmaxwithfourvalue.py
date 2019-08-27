ea1244afc8e540f828eca86e6327a8c3e24ee258
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-27 12:05:33 +0530

    pack arthmetic operation corrected

--- actions/minmaxwithfourvalue.py
+++ actions/minmaxwithfourvalue.py
@@ -10,10 +10,28 @@ class MyAction(Action):
             elif d<b and d<c:
                 print("d is lowest value")
             print("a greatest value")                 
-        elif b>a and b>c and b>d:                
+        elif b>a and b>c and b>d:      
+            if a<c and a<d:
+                print("a is lowest value")
+            elif c<a and c<d:
+                print("c is lowest value")
+            elif d<a and d<c:
+                print("d is lowest value")          
             print("b is greate than a c and d")
-        elif c>a and c>d and c>d:                
+        elif c>a and c>d and c>d:  
+            if a<b and a<d:
+                print("a is lowest value")
+            elif b<a and b<d:
+                print("b is lowest value")
+            elif d<a and d<c:
+                print("d is lowest value")              
             print("c is greate than a b and d")
         elif d>a and d>b and d>c: 
+            if a<c and a<b:
+                print("a is lowest value")
+            elif b<a and b<c:
+                print("b is lowest value")
+            elif c<a and c<b:
+                print("d is lowest value")
             print("d is greate than a b and c")
         return(True)
