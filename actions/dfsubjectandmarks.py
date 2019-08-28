8ff8a000cb8bb794b8cddbb13b4b54901384f5b4
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-28 16:56:22 +0530

    pack arthmetic operation corrected

--- actions/dfsubjectandmarks.py
+++ actions/dfsubjectandmarks.py
@@ -1,8 +1,20 @@
 import sys
 from st2common.runners.base_action import Action
 class MyAction(Action):
-    def run(self,a,b,c,d,e):     
+    def isNumber(self,s) :       
+        for i in range(len(s)) : 
+            if s[i].isdigit() != True : 
+                return False
+        return True
+    def run(self,a,b,c,d,e): 
+        bvalue=b
+        if self.isNumber(bvalue):
+            print("integer")
+        else:
+            print("string")
+
         dictionary1 = dict()
+        
         dictionary1.update({a: {b:c,d:e}})
         # dictionary1.update({a: {d:e}})   
         print("Student Subject and marks") 
