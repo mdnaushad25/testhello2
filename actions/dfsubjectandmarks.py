033e1fdc5300f200bcd6f478ccebd32814c77df0
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-28 16:17:00 +0530

    pack arthmetic operation corrected

--- actions/dfsubjectandmarks.py
+++ actions/dfsubjectandmarks.py
@@ -3,8 +3,8 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
     def run(self,a,b,c,d,e):     
         dictionary1 = dict()
-        dictionary1.update(a: {b:c})
-        dictionary1.update(a: {d:e})   
+        dictionary1.update({a: {b:c}})
+        dictionary1.update({a: {d:e}})   
         print("Student Subject and marks") 
     
         print(dictionary1) 
