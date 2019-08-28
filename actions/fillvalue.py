9e0c142fc366b49db462165df53aa48d0bb25cd4
Naushad Ansari <naushada@nihilentanalytics.com>
2019-08-28 15:32:11 +0530

    pack arthmetic operation corrected

--- actions/fillvalue.py
+++ actions/fillvalue.py
@@ -2,11 +2,12 @@ import sys
 from st2common.runners.base_action import Action
 class MyAction(Action):
     def run(self,a,b):
-        Dictionary1 = {'A': 'Geeks', 'B': 'For'}     
+        #Dictionary1 = {'A': 'Geeks', 'B': 'For'}     
         # Printing keys of dictionary 
+        Dictionary1 = {a: b, c:d}   
         print("Keys before Dictionary Updation:") 
-        keys = Dictionary1.keys() 
-        print(keys) 
+        #keys = Dictionary1.keys() 
+       # print(keys) 
         print(Dictionary1) 
         return(True)
     
